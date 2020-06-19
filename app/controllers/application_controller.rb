bc17faff1d828e7f8c1cc773f9f48b57069509e2
Chris Groves <chris_groves87@hotmail.co.uk>
2020-06-19 16:34:35 +0100

    change route to "goodbye, world!"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hola, mundo!"
+    render html: "hello, world!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
